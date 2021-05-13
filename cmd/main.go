6c924086f531cea930883d0206d1591afa55577b
saparatayev <sopa.97@mail.ru>
2021-05-13 16:00:10 +0500

    dependency injection

--- cmd/main.go
+++ cmd/main.go
@@ -4,10 +4,14 @@ import (
 	"log"
 	"zhashkRestApi"
 	"zhashkRestApi/pkg/handler"
+	"zhashkRestApi/pkg/repository"
+	"zhashkRestApi/pkg/service"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
 
 	srv := new(zhashkRestApi.Server)
 	if err := srv.Run("8081", handlers.InitRoutes()); err != nil {
