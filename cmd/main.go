6b1b4c9ca81e01c06b06efc066ae35b4ebefe8ff
saparatayev <sopa.97@mail.ru>
2021-05-14 11:33:33 +0500

    add config files

--- cmd/main.go
+++ cmd/main.go
@@ -6,15 +6,27 @@ import (
 	"zhashkRestApi/pkg/handler"
 	"zhashkRestApi/pkg/repository"
 	"zhashkRestApi/pkg/service"
+
+	"github.com/spf13/viper"
 )
 
 func main() {
+	if err := InitConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
+
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	srv := new(zhashkRestApi.Server)
-	if err := srv.Run("8081", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("8080"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
+}
+
+func InitConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
